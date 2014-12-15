f544e1de5454cd7c07022a342fa33b58fe39a414
Alex Gutler <alex11jgt@hotmail.com>
2014-12-15 21:50:22 -0200

    ajustei o array

--- public/index.php
+++ public/index.php
@@ -3,22 +3,22 @@
 require_once __DIR__.'/../bootstrap.php';
 
 $clientes = array(
-    0 => array(
+    array(
         'nome' => 'Alex Gutler',
         'email' => 'alex@grupolima.net.br',
         'cpf-cnpj' => 13535524709
     ),
-    1 => array(
+    array(
         'nome' => 'Renan Pissaia',
         'email' => 'renan@email.com.br',
         'cpf-cnpj' => 45678912332
     ),
-    2 => array(
+    array(
         'nome' => 'Henrique Biasuti',
         'email' => 'henrique@email.com.br',
         'cpf-cnpj' => 45625893612
     ),
-    3 => array(
+    array(
         'nome' => 'Simone Sousa',
         'email' => 'simone@email.com.br',
         'cpf-cnpj' => 74125896312
