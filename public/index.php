4ef537c180912b0ec84c17940af96489e2398d11
AlexGutler <alex11jgt@hotmail.com>
2014-12-26 11:57:05 -0200

    ajustei o index

--- public/index.php
+++ public/index.php
@@ -10,14 +10,17 @@ use AG\Produto\Entity\Produto;
 use AG\Produto\Mapper\ProdutoMapper;
 use AG\Produto\Service\ProdutoService;
 
+//
 $app['produto'] = function(){ return new Produto(); };
 $app['mapper'] = function() use ($app) { return new ProdutoMapper($app['conn']);};
 
+// armazenar o service do produto
 $app['produtoService'] = function() use ($app)
 {
     return new ProdutoService($app['produto'], $app['mapper']);
 };
 
+// rota para listar dos produtos
 $app->get("/produtos", function() use ($app)
 {
     /*
@@ -36,11 +39,19 @@ $app->get("/produtos", function() use ($app)
 })->bind('produtos')
 ;
 
+// rota para criar novo produto
 $app->get("/produtos/novo", function() use($app){
     return $app['twig']->render('produto-novo.twig', []);
 })->bind('produto-novo')
 ;
 
+// rota para editar um produto
+$app->get("/produtos/{id}/editar", function($id) use($app){
+    return $app['twig']->render('produto-editar.twig', ['id' => $id]);
+})->bind('produto-editar')
+;
+
+// rota index
 $app->get("/", function() use($app){
     return $app['twig']->render('index.twig', []);
 })->bind('index')
