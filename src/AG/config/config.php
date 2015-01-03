421e46048c1118dc4e67eaed5a184fd910929573
Alex Gutler <alex11jgt@hotmail.com>
2015-01-03 12:41:48 -0200

    ajustando a conexao

--- src/AG/config/config.php
+++ src/AG/config/config.php
@@ -7,6 +7,6 @@ return array(
         'dbname' => 'silexdb',
         'user' => 'root',
         'password' => '00fb00',
-        'dns' => 'mysql:host=localhost; port=3306; dbname=silexdb'
+        'dsn' => 'mysql:host=localhost; port=3306; dbname=silexdb'
     )
 );
